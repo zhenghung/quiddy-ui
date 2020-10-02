a93033f427986b61cf8eb7ffdc6204145eeeaa04
Zheng Hung Chuah <zheng_hung@hotmail.com>
2020-10-02 23:17:55 +0100

    feat!: webpack config set up

---
+++ src/components/AppRoot.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AppRoot.css';
+
+function AppRoot() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          HELLO WORLD
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default AppRoot;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    }],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico',
+            filename: './index.html',
+        }),
+        new CopyPlugin([
+            {from: 'public/robots.txt', to: 'robots.txt'},
+        ]),
+    ],
+};
