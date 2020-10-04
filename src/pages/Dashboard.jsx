c351882c7dd93bdddc865a8ef678f95de1813b4f
Zheng Hung Chuah <zheng_hung@hotmail.com>
2020-10-04 11:43:45 +0100

    Add bootstrap

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {PageHeader} from '../resources/constants';
+import Table from 'react-bootstrap/Table';
 
 function Dashboard({setPage}) {
 
@@ -7,7 +8,35 @@ function Dashboard({setPage}) {
 
     return (
         <div className='dashboard'>
-            <h1>Dashboard</h1>
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+                </tbody>
+            </Table>
         </div>
     );
 }
